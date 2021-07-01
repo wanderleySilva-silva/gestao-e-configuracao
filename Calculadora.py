1229be4128d5d13f721f79f171bddca1a07bb764
Cloves Jose <clovesjsilva8@gmail.com>
2021-07-01 08:30:11 -0300

    primeiro commit

---
+++ Calculadora.py
@@ -0,0 +1,61 @@
+from Operacao import Operacao
+
+class Calculadora:
+
+    flag = True
+
+    opr = Operacao
+
+    while flag:
+
+        print('Calculadora Python\n' + 
+            '1. Soma\n' +
+            '2. Subtracao\n' +
+            '3. Multiplicacao\n' +
+            '4. Divisao\n' +
+            '5. Potencia\n' +
+            '6. Sair')
+
+        opcao = input('Escolha uma das opções listadas: ')
+
+        if(opcao == '1'):
+            a = int(input('Digite o primeiro número: '))
+            b = int(input('Digite o segundo número: '))
+            print()
+
+            print(opr.soma(a, b))
+        
+        elif(opcao == '2'):
+            a = int(input('Digite o primeiro número: '))
+            b = int(input('Digite o segundo número: '))
+            print()
+
+            print(opr.subtracao(a, b))
+
+        elif(opcao == '3'):
+            a = int(input('Digite o primeiro número: '))
+            b = int(input('Digite o segundo número: '))
+            print()
+
+            print(opr.multiplicacao(a, b))
+        
+        elif(opcao == '4'):
+            a = int(input('Digite o primeiro número: '))
+            b = int(input('Digite o segundo número: '))
+            print()
+
+            print(opr.divisao(a, b))
+        
+        elif(opcao == '5'):
+            a = int(input('Digite o primeiro número: '))
+            b = int(input('Digite o segundo número: '))
+            print()
+
+            print(opr.potencia(a, b))
+        
+        elif(opcao == '6'):
+            flag = False
+
+        
+
+    
---
+++ Operacao.py
@@ -0,0 +1,22 @@
+class Operacao:
+
+    def soma(a, b):
+        return a + b
+    
+    def subtracao(a, b):
+        return a - b
+
+    def multiplicacao(a, b):
+        return a * b
+    
+    def divisao(a, b):
+        if(b == 0):
+            return 'A operação não pode ser efetuada'
+        else:
+            return a / b
+    
+    def potencia(a, b):
+        if(b == 0):
+            return 1
+        else:
+            return a**b
